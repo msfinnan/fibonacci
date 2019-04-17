83c47007b500e2c31418cddd00985ecc97c50c7d
Margaret Finnan <msfinnan@gmail.com>
2019-04-16 20:14:31 -0700

    completed method

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,8 +5,19 @@
 # ....
 # e.g. 6th fibonacci number is 8
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: Time complexity is linear, or O(n) where n is the number input into the method. This method will always run n times, so if n is 5 it will run 5 times and if n is 5000 it will run 5000 times.
+# Space complexity: Space complexity is constant or O(1) since there are three variables, no matter the size of n. The number of variables does not grow with the input.
+
 def fibonacci(n)
-  raise NotImplementedError
+  if n == nil || n < 0
+    raise ArgumentError, "Enter a positive interger"
+  end
+  first = 0
+  second = 1
+  n.times do
+    third = first + second
+    first = second
+    second = third
+  end
+  return first
 end
